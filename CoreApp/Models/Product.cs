87e3c9b9d7f12465810b8f76a6ef99134034c53a
emaru <emaru@DESKTOP-SSL64OD>
2020-11-27 01:31:13 +0800

    My First Commit

---
+++ CoreApp/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string PCode { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Category { get; set; }
+    }
+}
