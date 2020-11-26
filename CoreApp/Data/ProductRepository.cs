4e49fcdd64b5e31402929d9cbb3b9dfb21f64f5f
emaru <emaru@DESKTOP-SSL64OD>
2020-11-27 01:56:09 +0800

    Configure Startup • Connection String • Register Interface and Repository

--- CoreApp/Data/ProductRepository.cs
+++ CoreApp/Data/ProductRepository.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using CoreApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CoreApp.Data
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        public Product Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Product Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Update(Product productChanges)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CoreApp/Startup.cs
+++ CoreApp/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,13 +15,21 @@ namespace CoreApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDBContext>(options => options.UseSqlServer(Configuration["ConnectionStrings"]));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
