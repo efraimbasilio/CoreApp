d45f96aaf2917268804f8c6db8ddcb0e9d81167f
emaru <emaru@DESKTOP-SSL64OD>
2020-11-27 01:39:41 +0800

    Add AppDBContext

---
+++ CoreApp/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
