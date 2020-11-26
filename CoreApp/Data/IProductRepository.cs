210320a7bf1e1823b82f0ba4a065fb4b213e9d3e
emaru <emaru@DESKTOP-SSL64OD>
2020-11-27 01:43:51 +0800

    Add Product Interrface

---
+++ CoreApp/Data/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Data
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+
+        Product GetById(int id);
+
+        Product Add(Product product);
+
+        Product Delete(int id);
+
+        Product Update(Product productChanges);
+    }
+}
