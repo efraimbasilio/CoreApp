3c1c09786741128befbfa599673e6380662fb0b1
emaru <emaru@DESKTOP-SSL64OD>
2020-11-27 12:10:10 +0800

    Added User Interface and Repository

---
+++ CoreApp/Data/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using CoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Data
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAll();
+
+        User GetById(int id);
+
+        User Add(User user);
+
+        void Delete(int id);
+
+        User Update(User useChanges);
+    }
+}
---
+++ CoreApp/Data/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using CoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDBContext _context;
+
+        public UserRepository(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public User Add([FromBody]User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User useChanges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
